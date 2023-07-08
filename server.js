39e5553b1dcbba05d2ccadeb389bc7e80c5b6192
felippe <felippe.oliveira@seatelecom.com.br>
2023-07-08 10:31:27 -0300

    v1

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+// Verbos HTPP
+// GET: Receber dados de um resource.
+// POST: Envia dados ou informações para serem processados por um resource.
+// PUT: Atualiza dados de um resource.
+// DELETE: Deleta um Resource
+
+app.get()
+app.post()
+app.put()
+app.delete()
+
+app.listen(3000,function(){
+    console.log("Server is running");
+})
