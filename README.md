04a09e8308964acba11cca2e1815caa30d3de725
felippe <felippe.oliveira@seatelecom.com.br>
2023-07-08 12:11:32 -0300

    update code

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# restful-js
+## RESTFUL-JS
+
+RESTful, aplicação dos padrões REST.
+
+## BOAS PRÁTICAS
+- Utilização de verbos HTTP nas requisições.
+- Padronização de singular ou plural nos ENDPOINTS
+
+###  VERBOS HTTP
+
+- GET: Recebe dados de um Resource;
+- POST: Envia dados ou informações para serem processados por um resource;
+- PUT: Atualiza dados de um Resource;
+- DELETE: Deleta um Resource;
+
+### STATUS RESPONSE
+
+ - 1xx: Information
+ - 2xx: Sucesso
+   - 200: OK
+   - 201: CREATED
+   - 204: No content
+ - 3xx: Redirection
+ - 4xx: Client Error
+   - 400: Bad Request
+   - 404: Not Found!
+ - 5xx: Server Error
+   - 500: Internal Server Error
--- server.js
+++ server.js
@@ -1,16 +1,52 @@
 const express = require('express');
 const app = express();
+const data = require("./data.json")
 
-// Verbos HTPP
-// GET: Receber dados de um resource.
-// POST: Envia dados ou informações para serem processados por um resource.
-// PUT: Atualiza dados de um resource.
-// DELETE: Deleta um Resource
+app.use(express.json());
 
-app.get()
-app.post()
-app.put()
-app.delete()
+
+app.get("/clients", function(req, res){
+    res.json(data);
+});
+
+app.get("/clients/:id", function(req, res){
+    const { id } = req.params
+    const client = data.find(cli => cli.id == id);
+
+    if (!client) return res.status(404).json();
+    
+    res.json(client)
+});
+
+app.post("/clients", function(req, res){
+    const { name, username, email } = req.body;
+   
+    res.json({ name, username, email });
+});
+
+app.put("/clients/:id", function(req, res){
+    const { id } = req.params;
+    const client = data.find(cli => cli.id == id);
+
+    if (!client) return res.status(204).json();
+
+    const { name } = req.body;
+    const { username } = req.body;
+    const { email } = req.body;
+    
+    client.username = username;
+    client.name = name;
+    client.email = email;
+
+    res.json(client);
+});
+
+app.delete("/clients/:id", function(req, res){
+    const { id } = req.params;
+    const clientsFiltered = data.filter(client => client.id != id);
+
+    res.json(clientsFiltered);
+});
 
 app.listen(3000,function(){
     console.log("Server is running");
